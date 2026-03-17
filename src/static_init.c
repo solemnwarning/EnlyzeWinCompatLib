159f0cc7acceb9cb5c9b46e4a2bc2ba8244d41cc
void_17 <heroerror3@gmail.com>
2026-03-17 20:39:14 +0700

    Use lightweight thread safe static init code
    This version is derived from CC0 implementation from ReactOS and NXDK: https://github.com/XboxDev/nxdk/tree/master (this implementation has been battle tested and actually works)
    
    It implements `_Init_thread_header`, `_Init_thread_footer` and `_Init_thread_abort` with atomics instead of heavy winpthreads dependencies.
    This also defeats the need in other functions in this file: `_Init_thread_lock`, `_Init_thread_unlock`, `_Init_thread_wait_v2`, `_Init_thread_notify` are not called by MSVC or Clang-cl frontends, source: https://github.com/llvm/llvm-project/blob/main/clang/lib/CodeGen/MicrosoftCXXABI.cpp
    Only `_Init_thread_epoch`, _Init_thread_header`, `_Init_thread_footer` and `_Init_thread_abort` are exposed for use with clang-cl fronend.

--- src/static_init.c
+++ src/static_init.c
@@ -1,104 +1,60 @@
-/* This file provides routines used for thread-safe initialisation of static
- * variables when /Zc:threadSafeInit is used (enabled by default).
- *
- * This is intended to override the default implementations from the Microsoft
- * C++ Runtime which are compiled to target Windows Vista or later.
- *
- * Modeled on the reference implementation in thread_safe_statics.cpp in the
- * Microsoft C++ Runtime.
-*/
+// SPDX-License-Identifier: CC0-1.0
 
-#include <assert.h>
+// SPDX-FileCopyrightText: 2019 Timo Kreuzer
+// SPDX-FileCopyrightText: 2020 Stefan Schmidt
+
+// Derived from ReactOS CRT code to provide thread safe statics initialization
+// in nxdk, as required by C++11.
+
+#include <stdint.h>
 #include <limits.h>
-#include <pthread.h>
+#include <windows.h>
 
-static const int UNINITIALIZED = 0;
-static const int INITIALIZING = -1;
 
-static const int EPOCH_BASE = INT_MIN;
+long _Init_global_epoch = LONG_MIN;
+__declspec(thread) long _Init_thread_epoch = LONG_MIN;
 
-/* Exposed as a public symbol in the reference implementation, so exposed it
- * has to stay here too...
+/*
+    This function tries to acquire a lock on the initialization for the static
+    variable by changing the value saved in *ptss to -1. If *ptss is 0, the
+    variable was not initialized yet and the function tries to set it to -1.
+    If that succeeds, the function will return. If the value is already -1,
+    another thread is in the process of doing the initialization and we
+    wait for it. If it is any other value the initialization is complete.
+    After returning the compiler generated code will check the value:
+    if it is -1 it will continue with the initialization, otherwise the
+    initialization must be complete and will be skipped.
 */
-int _Init_global_epoch = EPOCH_BASE;
-__declspec(thread) int _Init_thread_epoch = EPOCH_BASE;
-
-static pthread_mutex_t _Init_thread_mutex = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t _Init_thread_cond = PTHREAD_COND_INITIALIZER;
-
-void __cdecl _Init_thread_lock()
+void _Init_thread_header (volatile int *ptss)
 {
-        pthread_mutex_lock(&_Init_thread_mutex);
-}
-
-void __cdecl _Init_thread_unlock()
-{
-        pthread_mutex_unlock(&_Init_thread_mutex);
-}
-
-void __cdecl _Init_thread_wait_v2()
-{
-        pthread_cond_wait(&_Init_thread_cond, &_Init_thread_mutex);
-}
-
-void __cdecl _Init_thread_notify()
-{
-        pthread_cond_broadcast(&_Init_thread_cond);
-}
-
-void __cdecl _Init_thread_header(int* const pOnce)
-{
-    _Init_thread_lock();
-
-    if (*pOnce == UNINITIALIZED)
-    {
-        *pOnce = INITIALIZING;
-    }
-    else
-    {
-        while (*pOnce == INITIALIZING)
-        {
-            _Init_thread_wait_v2();
-
-            if (*pOnce == UNINITIALIZED)
-            {
-                *pOnce = INITIALIZING;
-                _Init_thread_unlock();
-                return;
-            }
+    while (1) {
+        /* Try to acquire the first initialization lock */
+        int oldTss = _InterlockedCompareExchange((long *)ptss, -1, 0);
+        if (oldTss == -1) {
+            /* Busy, wait for the other thread to do the initialization */
+            SwitchToThread();
+            continue;
+        } else if (oldTss == 0) {
+            /* We acquired the lock and the caller will do the initialization */
+            return;
         }
+
+        /* The initialization is complete and the caller will skip it.
+           Update the epoch so this call can be skipped in the future, it
+           only needs to run once per thread. */
         _Init_thread_epoch = _Init_global_epoch;
+        return;
     }
-
-    _Init_thread_unlock();
 }
 
-void __cdecl _Init_thread_abort(int* const pOnce)
+void _Init_thread_footer (volatile int *ptss)
 {
-    _Init_thread_lock();
-
-    *pOnce = UNINITIALIZED;
-
-    _Init_thread_unlock();
-    _Init_thread_notify();
+    /* Initialization is complete */
+    _Init_thread_epoch = *ptss = _InterlockedIncrement(&_Init_global_epoch);
 }
 
-void __cdecl _Init_thread_footer(int* const pOnce)
+void _Init_thread_abort (volatile int *ptss)
 {
-    _Init_thread_lock();
-
-    ++_Init_global_epoch;
-
-    /* Probably unlikely condition... you would need to construct ~2 billion
-     * static objects before the "epoch" would roll up to the "INITIALIZING"
-     * constant and cause weird behaviour... the official implementation
-     * technically has this bug too.
-    */
-    assert(_Init_global_epoch < INITIALIZING);
-
-    *pOnce = _Init_global_epoch;
-    _Init_thread_epoch = _Init_global_epoch;
-
-    _Init_thread_unlock();
-    _Init_thread_notify();
+    /* Abort the initialization */
+    _InterlockedAnd((volatile long *)ptss, 0);
 }
